fac50535403a747f07d7d242272a8a8346a50cab
salmanee <s.almanee_it@yahoo.com>
2021-01-16 20:51:46 -0800

    added app bins

--- scripts/clusters_libs.py
+++ scripts/clusters_libs.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Extracts biniares from apps and clusters them based on their sha256
+__author__ = "Sumaya Almanee"
+__credits__ = ["Arda Unal", "Mathias Payer" ,"Joshua Garcia"] 
+__copyright__ = "Copyright 2021, EPFL and UCI"
+__license__ = "GPL"
+__version__ = "2.0"
 
+#Extracts biniares from apps and clusters them based on their sha256
 import os
 import subprocess
 import json
@@ -11,7 +16,7 @@ import shutil
 import time
 
 apps_dir="../sample_apps"
-dest_folder = "UnKnownLibs_bins"
+dest_folder = "../UnknownLibs_bins"
 start_time=time.time()
 
 for app in os.listdir(apps_dir):
@@ -31,9 +36,10 @@ for app in os.listdir(apps_dir):
 
                     # Create a new directory with binary hash_code (if such folder does not exist), and copy binary to that folder 
                     renamed_native=binary[:-3]+"_"+app+".so"
-                    if(not(os.path.exists(os.path.join(dest_folder,cluster_name)))):
-                        os.mkdir(os.path.join(dest_folder,cluster_name))
-                    #shutil.move(os.path.join("temp",binary),os.path.join(cluster_name,renamed_native))
+                    cluster_name=os.path.join(dest_folder,cluster_name)
+                    if(not(os.path.exists(cluster_name))):
+                        os.mkdir(cluster_name)
+                    shutil.move(os.path.join("temp",binary),os.path.join(cluster_name,renamed_native))
         subprocess.call(["rm","-r","temp"])
 
 end_time=time.time()
