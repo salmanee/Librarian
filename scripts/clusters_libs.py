ba1923c56e62d26ad5f01f7a82d437832913b74b
root <root@sora2.ics.uci.edu>
2021-01-16 20:23:06 -0800

    minoor changes to cluster_libs

--- scripts/clusters_libs.py
+++ scripts/clusters_libs.py
@@ -10,30 +10,31 @@ import hashlib
 import shutil
 import time
 
-apps_dir="../downloaded_apks_AndroZoo_May-19-2020"
-destination_folder = "../KnownLibs_bins"
-start=time.time()
+apps_dir="../sample_apps"
+dest_folder = "UnKnownLibs_bins"
+start_time=time.time()
 
-#iterates and unzips each .apk per app
-#for each binary under /lib, generate a hash code that corresponds to binary
-#create a new directory with binary hash_code (if such folder does not exist), and copy binary to that folder 
-for app, root, versions in os.walk(apps_dir):
-        for version in versions:
-                if version.endswith(".apk"):
-                        version_path=os.path.join(app,version)
-                        apk_version = version_path[:-4]
-                        subprocess.call(["unzip -j -o %s %s -d %s"%(version_path.replace("\"","\\\""), "lib/*",apk_version.replace("\"","\\\""))],shell = True)
+for app in os.listdir(apps_dir):
+    if app.endswith(".apk"):
+        # Iterates and unzips each .apk per app
+        subprocess.call(["unzip -j -o %s %s -d %s" % (os.path.join(apps_dir,app),"lib/*","temp")],
+                        shell = True,
+                        stdout=subprocess.DEVNULL, 
+                        stderr=subprocess.STDOUT)
 
-                        for file in os.listdir(apk_version):
-                                if file.endswith(".so"):
-                                        filename = os.path.join(apk_version, file)
-                                        with open(filename,"rb") as f:
-                                                bytes = f.read() # read entire file as bytes                                                                                                                                                        readable_hash = hashlib.sha256(bytes).hexdigest()
-                                        cluster_name=readable_hash
-                                        renamed_native=os.path.splitext(file)[0]+"_"+os.path.splitext(version)[0]+".so"
-                                        if(not(os.path.exists(cluster_name))):
-                                                os.mkdir(cluster_name)
-                                        shutil.move(filename,cluster_name+"/"+renamed_native)
-                        subprocess.call(["rm","-r",apk_version])
-end=time.time()
-print("total execution time:{} sec ".format(end-start))
+        for binary in os.listdir("temp"):
+            if binary.endswith(".so"):
+                with open(os.path.join("temp",binary),"rb") as f:
+                    # For each binary under /lib, generate its corresponding hash code
+                    bytes = f.read() 
+                    cluster_name = hashlib.sha256(bytes).hexdigest()
+
+                    # Create a new directory with binary hash_code (if such folder does not exist), and copy binary to that folder 
+                    renamed_native=binary[:-3]+"_"+app+".so"
+                    if(not(os.path.exists(os.path.join(dest_folder,cluster_name)))):
+                        os.mkdir(os.path.join(dest_folder,cluster_name))
+                    #shutil.move(os.path.join("temp",binary),os.path.join(cluster_name,renamed_native))
+        subprocess.call(["rm","-r","temp"])
+
+end_time=time.time()
+print("total execution time:{:.2f} sec".format(end_time-start_time))
